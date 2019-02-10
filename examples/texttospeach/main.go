a1a49892076e5d05d9b72a45d8476397001af4f9
Evgeny Safonov <ya.zver@gmail.com>
2019-02-10 18:44:36 +0700

    Add example

---
+++ examples/texttospeach/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/yazver/yaspeech"
+)
+
+func main() {
+	oauth := os.Getenv("OAUTH")
+	folderID := os.Getenv("FOLDER_ID")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	yaspeech.InitIAMTokenContext(ctx, oauth)
+	tts := yaspeech.NewTextToSpeech(folderID)
+
+	audio, err := tts.Synthesize("Съешь ещё этих мягких французских булок, да выпей же чаю.")
+	if err != nil {
+		fmt.Println("Unable to convert:", err)
+		return
+	}
+	if err := ioutil.WriteFile("audio.ogg", audio, 0644); err != nil {
+		return
+	}
+
+}
